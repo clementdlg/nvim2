b905b9afde6650ab17ee2a011bbad3fe0e4f4e80
Clément DE LA GENIERE <0xkrem@proton.me>
2025-10-21 21:24:50 +0200

    reduced blink.cmp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,17 +43,8 @@ vim.diagnostic.config {
 
 -- [[ Completion ]]
 require('blink.cmp').setup {
-	keymap = {
-		preset = 'default',
-	},
-	appearance = {
-		nerd_font_variant = 'mono',
-	},
-	completion = {
-		documentation = { auto_show = false, auto_show_delay_ms = 500 },
-	},
 	sources = {
-		default = { 'lsp', 'path' },
+		default = { 'lsp', 'path' }
 	},
 	fuzzy = { implementation = 'lua' },
 	signature = { enabled = true },
