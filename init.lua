01b855952764422fabe83a034eaddf1a0480ac2f
Clément DE LA GENIERE <0xkrem@proton.me>
2025-10-26 02:44:28 +0100

    init.lua get env variable for runtime path

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+local cfg_path = os.getenv("NVIM_CFG")
+
+if cfg_path then
+  vim.opt.runtimepath:prepend(cfg_path)
+else
+  print("NVIM_CFG not set")
+end
+
 print('hello world')
 -- [[ Plugin config ]]
 require 'plugins.lazy'
