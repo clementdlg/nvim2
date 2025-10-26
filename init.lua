dc765aade94d3c96b1bfb8f7b082d889247af6c3
Clément DE LA GENIERE <0xkrem@proton.me>
2025-10-26 07:35:46 +0100

    fixed init.lua

--- init.lua
+++ init.lua
@@ -4,9 +4,28 @@ require 'keymaps'
 require 'core-lsp'
 
 -- [[ Plugin config ]]
-require 'plugins.colorscheme'
-require 'plugins.statusline'
-require 'plugins.git'
-require 'plugins.fuzzyfind'
-require 'plugins.lsp'
-require 'plugins.treesitter'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	-- 🌙 Tokyonight colorscheme
+	{
+		'folke/tokyonight.nvim',
+		priority = 1000,
+		config = function()
+			require('tokyonight').setup {
+				style = 'night',
+				transparent = true,
+			}
+
+			vim.cmd.colorscheme('tokyonight')
+		end
+	},
+})
+
+-- require 'plugins.colorscheme'
+-- require 'plugins.statusline'
+-- require 'plugins.git'
+-- require 'plugins.fuzzyfind'
+-- require 'plugins.lsp'
+-- require 'plugins.treesitter'
