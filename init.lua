f900c0ec1554e19fd1ae203dab8433737b13e1f4
Clément DE LA GENIERE <0xkrem@proton.me>
2025-10-26 03:10:53 +0100

    wip

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
-local cfg_path = os.getenv("NVIM_CFG")
+-- local cfg_path = os.getenv("NVIM_CFG")
+--
+-- if cfg_path then
+--   vim.opt.runtimepath:append(cfg_path)
+-- else
+--   print("NVIM_CFG not set")
+-- end
 
-if cfg_path then
-  vim.opt.runtimepath:append(cfg_path)
-else
-  print("NVIM_CFG not set")
-end
+vim.opt.path:append('**')
 
 -- [[ Plugin config ]]
 require 'plugins.lazy'
