2e504150b01059a91c7ff31f49070b99f0d45af9
Clément DE LA GENIERE <0xkrem@proton.me>
2025-10-26 03:03:49 +0100

    fix lazy.lua

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,15 +1,10 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = os.getenv("LAZY")
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
+    vim.api.print("Failed to clone lazy.nvim")
     os.exit(1)
   end
 end
